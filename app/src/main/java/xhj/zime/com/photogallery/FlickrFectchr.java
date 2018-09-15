71e2aa0e9bdca6be2a1cf1dea5cdfe086e47517d
谢浩杰 <1012351395@qq.com>
2018-09-15 17:12:26 +0800

    使用异步任务从网络上获取百度图片的标题并显示在列表中

--- app/src/main/java/xhj/zime/com/photogallery/FlickrFectchr.java
+++ app/src/main/java/xhj/zime/com/photogallery/FlickrFectchr.java
@@ -1,4 +1,88 @@
 package xhj.zime.com.photogallery;
 
+import android.net.Uri;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
 public class FlickrFectchr {
+
+    private static final String TAG = "FlickrFectchr";
+
+    public void parseItems(List<GalleryItem> items,JSONObject jsonBody) throws JSONException {
+        JSONArray photoJsonArray = jsonBody.getJSONArray("data");
+        for (int i = 0; i < photoJsonArray.length() ; i++){
+            JSONObject photoJsonObject = photoJsonArray.getJSONObject(i);
+            GalleryItem item = new GalleryItem();
+            item.setId(photoJsonObject.getString("id"));
+            item.setCaption(photoJsonObject.getString("abs"));
+            if (!photoJsonObject.has("image_url")){
+                continue;
+            }
+            item.setUrl(photoJsonObject.getString("image_url"));
+            items.add(item);
+        }
+    }
+
+
+    public List<GalleryItem> fetchItems(){
+        List<GalleryItem> items = new ArrayList<>();
+        String url = Uri.parse("http://image.baidu.com/channel/listjson/")
+                .buildUpon()
+                .appendQueryParameter("pn","0")
+                .appendQueryParameter("rn","30")
+                .appendQueryParameter("tag1","明星")
+                .appendQueryParameter("tag2","全部")
+                .appendQueryParameter("ftags","女明星##内地")
+                .appendQueryParameter("ie","utf8")
+                .build().toString();
+        try {
+            String jsonString = getUrlString(url);
+            JSONObject jsonObject = new JSONObject(jsonString);
+            parseItems(items,jsonObject);
+            Log.i(TAG, "Received JSON: "+jsonString);
+        } catch (IOException e) {
+            Log.i(TAG, "Failed to fetch items",e);
+        } catch (JSONException e) {
+            Log.i(TAG, "Failed to parse JSON",e);
+        }
+        return items;
+    }
+
+    public byte[] getUrlBytes(String urlSpec) throws IOException {
+        URL url = new URL(urlSpec);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        try {
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            InputStream in = connection.getInputStream();
+            if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+                throw new IOException(connection.getResponseMessage() +
+                        ": with " +
+                        url);
+            }
+            int bytesRead = 0;
+            byte[] buffer = new byte[1024];
+            while ((bytesRead = in.read(buffer)) > 0) {
+                out.write(buffer, 0, bytesRead);
+            }
+            out.close();
+            return out.toByteArray();
+        } finally {
+            connection.disconnect();
+        }
+    }
+
+    public String getUrlString(String urlSpec) throws IOException {
+        return new String(getUrlBytes(urlSpec));
+    }
 }
--- app/src/main/java/xhj/zime/com/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/xhj/zime/com/photogallery/PhotoGalleryFragment.java
@@ -1,5 +1,6 @@
 package xhj.zime.com.photogallery;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -9,26 +10,96 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PhotoGalleryFragment extends Fragment {
+
+    private static final String TAG = "PhotoGalleryFragment";
+
     private RecyclerView mPhotoRecyclerView;
+
+    private List<GalleryItem> mItems = new ArrayList<>();
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRetainInstance(true);
+        new FetchItemsTask().execute();
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_photo_gallery,container,false);
+        View view = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
         mPhotoRecyclerView = view.findViewById(R.id.photo_recycler_view);
-        mPhotoRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(),3));
+        mPhotoRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+        setupAdapter();
         return view;
     }
 
-    public static PhotoGalleryFragment newInstance(){
+    private void setupAdapter() {
+        if (isAdded()){
+            mPhotoRecyclerView.setAdapter(new PhotoAdapter(mItems));
+        }
+    }
+
+    public static PhotoGalleryFragment newInstance() {
         return new PhotoGalleryFragment();
+    }
+
+    private class FetchItemsTask extends AsyncTask<Void, Void, List<GalleryItem>> {
+
+        @Override
+        protected List<GalleryItem> doInBackground(Void... voids) {
+            return new FlickrFectchr().fetchItems();
+        }
+
+        @Override
+        protected void onPostExecute(List<GalleryItem> items) {
+            mItems = items;
+            setupAdapter();
+        }
+    }
+
+    private class PhotoAdapter extends RecyclerView.Adapter<PhotoHolder>{
+        private List<GalleryItem> mGalleryItems;
+
+        public PhotoAdapter(List<GalleryItem> galleryItems) {
+            mGalleryItems = galleryItems;
+        }
+
+        @NonNull
+        @Override
+        public PhotoHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+            TextView textView = new TextView(getActivity());
+            return new PhotoHolder(textView);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull PhotoHolder photoHolder, int i) {
+            GalleryItem galleryItem = mGalleryItems.get(i);
+            photoHolder.bindGalleryItem(galleryItem);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mGalleryItems.size();
+        }
+    }
+
+    private class PhotoHolder extends RecyclerView.ViewHolder{
+        private TextView mTitleTextView;
+
+        public PhotoHolder(@NonNull View itemView) {
+            super(itemView);
+            mTitleTextView = (TextView) itemView;
+        }
+        public void bindGalleryItem(GalleryItem item){
+            mTitleTextView.setText(item.toString());
+        }
     }
 }
